1d71a30c21307d62c1f9f4dbb4ca6bca0e823908
zaxlct <zaxlct@foxmail.com>
2019-07-02 15:37:08 +0800

    add packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Egrid from 'egrid'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, {  size: 'small' })
+Vue.use(Egrid)
 
 Vue.config.productionTip = false
 
